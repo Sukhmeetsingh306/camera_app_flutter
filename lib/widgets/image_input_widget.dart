85bd783d36a5574e9f43c39e683adee6c6cbc248
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2024-03-17 15:47:00 +0530

    enabled the camera to take the pictures

--- lib/widgets/image_input_widget.dart
+++ lib/widgets/image_input_widget.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInputWidget extends StatefulWidget {
   const ImageInputWidget({super.key});
@@ -8,12 +11,46 @@ class ImageInputWidget extends StatefulWidget {
 }
 
 class _ImageInputWidgetState extends State<ImageInputWidget> {
+  // for making the image taken to be seen
+  File? _selectedImage;
 
   // this will the code  in which the camera will open and take the input
-  void _takePicture() {}
+  void _takePicture() async {
+    final imagePicker = ImagePicker();
+    final pickedImage = await imagePicker.pickImage(
+      source: ImageSource.camera,
+      maxWidth: 600,
+    );
+
+    if (pickedImage == null) {
+      return;
+    }
+
+    setState(() {
+      _selectedImage = File(pickedImage.path); // to take to the picture
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    Widget content = TextButton.icon(
+      onPressed: _takePicture,
+      icon: const Icon(Icons.camera),
+      label: const Text('Take Picture'),
+    );
+
+    if (_selectedImage != null) {
+      content = GestureDetector(
+        onTap: _takePicture,
+        child: Image.file(
+          _selectedImage!,
+          fit: BoxFit.cover,
+          width: double.infinity,
+          height: double.infinity,
+        ),
+      );
+    }
+
     return Container(
       decoration: BoxDecoration(
         border: Border.all(
@@ -24,11 +61,7 @@ class _ImageInputWidgetState extends State<ImageInputWidget> {
       height: 200,
       width: double.infinity,
       alignment: Alignment.center,
-      child: TextButton.icon(
-        onPressed: _takePicture,
-        icon: const Icon(Icons.camera),
-        label: const Text('Take Picture'),
-      ),
+      child: content,
     );
   }
 }
