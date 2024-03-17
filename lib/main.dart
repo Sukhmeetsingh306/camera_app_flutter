53d1dba820629b53fa880883104e15a00b8f6771
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2024-03-17 13:39:30 +0530

    Added the right riverpod and the riverpod file and the code and alter the main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
-import 'package:camera_flutter/screen/place_list_screen.dart';
 import 'package:flutter/material.dart';
-
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:camera_flutter/screen/place_list_screen.dart';
 
 final colorScheme = ColorScheme.fromSeed(
   brightness: Brightness.dark,
@@ -27,7 +28,9 @@ final theme = ThemeData().copyWith(
 
 void main() {
   runApp(
-    const MyApp(),
+    const ProviderScope(
+      child: MyApp(),
+    ),
   );
 }
 
---
+++ lib/providers/user_place_provider.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../models/place_model.dart';
+
+class UserPlaceProviderNotifier extends StateNotifier<List<PlaceModel>>{
+  UserPlaceProviderNotifier(): super(const []);
+  
+  void addPlaceUserProvider(String title){
+    final newPlace =  PlaceModel(title: title);
+    state = [...state,newPlace];// ...state this will update the state and not delete the old data
+  }
+}
+
+final userPlaceProvider = StateNotifierProvider((ref) => UserPlaceProviderNotifier(),);
