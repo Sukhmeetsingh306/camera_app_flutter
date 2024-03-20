26799fb50bcb60d639a5a62dfea310a726123150
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2024-03-20 22:54:39 +0530

    added the modal for the location and made the location to be saved

--- lib/models/place_model.dart
+++ lib/models/place_model.dart
@@ -4,10 +4,27 @@ import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
-class  PlaceModel {
+class PlaceModel {
   final String id;
   final String title;
   final File image;
+  final PlaceLocationModal location;
 
-  PlaceModel( {required this.image,required this.title}) : id = uuid.v4();
+  PlaceModel({
+    required this.location,
+    required this.image,
+    required this.title,
+  }) : id = uuid.v4();
+}
+
+class PlaceLocationModal {
+  final double latitude;
+  final double longitude;
+  final double address;
+
+  PlaceLocationModal({
+    required this.latitude,
+    required this.longitude,
+    required this.address,
+  });
 }
