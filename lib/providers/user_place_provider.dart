e6625394d5869107e7bf24adbd173b9419e7d129
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2024-03-24 13:37:53 +0530

    made some changes in the provider for storing the data

--- lib/providers/user_place_provider.dart
+++ lib/providers/user_place_provider.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:path_provider/path_provider.dart' as syspaths;
+import 'package:path/path.dart' as path;
 
 import '../models/place_model.dart';
 
@@ -8,10 +10,15 @@ class UserPlaceProviderNotifier extends StateNotifier<List<PlaceModel>> {
   UserPlaceProviderNotifier() : super(const []);
 
   void addPlaceUserProvider(
-      String title, File image, PlaceLocationModal location) {
+      String title, File image, PlaceLocationModal location)  async{
+    //code for the path to store the data on to the device
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final fileName =  path.basename(image.path);
+    final copyImage = await image.copy('${appDir.path}/$fileName');
+
     final newPlace = PlaceModel(
       title: title,
-      image: image,
+      image: copyImage,
       location: location,
     );
     state = [
